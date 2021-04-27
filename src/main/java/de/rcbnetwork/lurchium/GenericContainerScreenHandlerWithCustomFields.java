0848024cd06c7053fed616130c8a6e7f05849fb3
Reinhard Bronner <bronner_reinhard@t-online.de>
2021-04-27 05:15:23 +0200

    Added support for drag and drop into lurchys chest.

---
+++ src/main/java/de/rcbnetwork/lurchium/GenericContainerScreenHandlerWithCustomFields.java
@@ -0,0 +1,7 @@
+package de.rcbnetwork.lurchium;
+
+import net.minecraft.entity.player.PlayerInventory;
+
+public interface GenericContainerScreenHandlerWithCustomFields {
+    PlayerInventory getPlayerInventory();
+}
--- src/main/java/de/rcbnetwork/lurchium/mixin/ScreenHandlerMixin.java
+++ src/main/java/de/rcbnetwork/lurchium/mixin/ScreenHandlerMixin.java
@@ -4,52 +4,34 @@ import de.rcbnetwork.lurchium.ChestBlockEntityWithCustomEvents;
 import de.rcbnetwork.lurchium.GenericContainerScreenHandlerWithCustomFields;
 import net.minecraft.block.entity.ChestBlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.screen.GenericContainerScreenHandler;
-import net.minecraft.screen.ScreenHandlerType;
+import net.minecraft.screen.ScreenHandler;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(GenericContainerScreenHandler.class)
-public class GenericContainerScreenHandlerMixin implements GenericContainerScreenHandlerWithCustomFields {
-    @Shadow
-    Inventory inventory;
-
-    @Unique
-    PlayerInventory playerInventory;
-
-    @Unique
-    public PlayerInventory getPlayerInventory() {
-        return playerInventory;
-    }
-
-    @Shadow
-    int rows;
-
-    @Inject(method="<init>(Lnet/minecraft/screen/ScreenHandlerType;ILnet/minecraft/entity/player/PlayerInventory;Lnet/minecraft/inventory/Inventory;I)V", at=@At("RETURN"))
-    void handleInit(ScreenHandlerType<?> type, int syncId, PlayerInventory playerInventory, Inventory inventory, int rows, CallbackInfo info) {
-        this.playerInventory = playerInventory;
-    }
-
+@Mixin(ScreenHandler.class)
+public class ScreenHandlerMixin {
     @Inject(method="*", at=@At(value = "INVOKE", target = "Lnet/minecraft/screen/slot/Slot;setStack(Lnet/minecraft/item/ItemStack;)V", shift = At.Shift.AFTER))
     void handleSetStack(CallbackInfoReturnable info) {
+        if (!((Object)this instanceof GenericContainerScreenHandler)) {
+            return;
+        }
+
+        Inventory inventory = ((GenericContainerScreenHandler)(Object)this).getInventory();
         if (!(inventory instanceof ChestBlockEntity)) {
             return;
         }
-        PlayerEntity player = playerInventory.player;
+        PlayerEntity player = ((GenericContainerScreenHandlerWithCustomFields)(Object)this).getPlayerInventory().player;
         World world = player.world;
         ChestBlockEntity chestBlockEntitiy = (ChestBlockEntity) inventory;
         ChestBlockEntityWithCustomEvents chestBlockEntitiyWCE = (ChestBlockEntityWithCustomEvents)chestBlockEntitiy;
         BlockPos pos = chestBlockEntitiy.getPos();
         chestBlockEntitiyWCE.getInventoryChangedEvent().trigger((GenericContainerScreenHandler)(Object)this, world, pos, player, chestBlockEntitiy);
     }
-
 }
